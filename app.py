5e38c9ec1cdefd3fa5c1b10bad5f15932eef7d9a
Deep903 <deeppatel55@gmail.com>
2019-11-12 13:38:11 -0600

    Base code for NB done

--- app.py
+++ app.py
@@ -12,6 +12,31 @@ def main_script():
     test = "Hello World!!!!"
     test = preprocess_string(test)
     print(test)
+    from sklearn.datasets import fetch_20newsgroups
+    categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+    newsgroups_train = fetch_20newsgroups(subset='train', categories=categories)
+    train_data = newsgroups_train.data  # getting all trainign examples
+    train_labels = newsgroups_train.target  # getting training labels
+    print ("Total Number of Training Examples: ",len(train_data)) # Outputs -> Total Number of Training Examples:  2257
+    print ("Total Number of Training Labels: ",len(train_labels)) # Outputs -> #Total Number of Training Labels:  2257
+    nb = NaiveBayes(np.unique(train_labels))  # instantiate a NB class object
+    print("---------------- Training In Progress --------------------")
+
+    nb.train(train_data, train_labels)  # start tarining by calling the train function
+    print('----------------- Training Completed ---------------------')
+    newsgroups_test = fetch_20newsgroups(subset='test', categories=categories)  # loading test data
+    test_data = newsgroups_test.data  # get test set examples
+    test_labels = newsgroups_test.target  # get test set labels
+
+    print("Number of Test Examples: ", len(test_data))  # Output : Number of Test Examples:  1502
+    print("Number of Test Labels: ", len(test_labels))  # Output : Number of Test Labels:  1502
+    pclasses = nb.test(test_data)  # get predcitions for test set
+
+    # check how many predcitions actually match original test labels
+    test_acc = np.sum(pclasses == test_labels) / float(test_labels.shape[0])
+
+    print("Test Set Examples: ", test_labels.shape[0])  # Outputs : Test Set Examples:  1502
+    print("Test Set Accuracy: ", test_acc * 100, "%")  # Outputs : Test Set Accuracy:  93.8748335553 %
     return 'Hello World!'
 
 
@@ -223,6 +248,7 @@ class NaiveBayes:
             predictions.append(self.classes[np.argmax(post_prob)])
 
         return np.array(predictions)
+
 
 if __name__ == '__main__':
     app.run()
